4830dabdc8f8b58f75b14de76ddf086b2f8ce35c
kurtjdyerly <kurtjdyerly@gmail.com>
2023-03-21 11:03:53 -0600

    learning prep 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square1 = new Square("Red", 5);
+
+        
+        Rectangle rectangle1 = new Rectangle("green", 5, 2);
+
+        
+        Circle cirlce1 = new Circle("blue", 3);
+
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square1);
+        shapes.Add(rectangle1);
+        shapes.Add(cirlce1);
+        foreach (Shape shape in shapes)
+        {
+        Console.WriteLine("The Color is " + shape.GetColor());
+        Console.Write("The area is ");
+        Console.WriteLine(shape.GetArea());
+        }
+
     }
 }
