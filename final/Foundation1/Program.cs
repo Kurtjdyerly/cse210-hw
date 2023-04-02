ecaa63e6b3977b1f1e0bff767db95d6a67613a46
kurtjdyerly <kurtjdyerly@gmail.com>
2023-04-01 20:34:56 -0600

    finished 1 and 2 of the foundation 4

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        Video video1 = new Video("OverSimplified","The War of the Bucket",1840);
+        video1.AddComment(new Comment("no","what is happing IN THE MIDDLE OF DA VID"));
+        video1.AddComment(new Comment(" Rem Crimson","Here we come REEEEEE!"));
+        video1.AddComment(new Comment("Henry Stracke","Hahahahah"));
+
+        Video video2 = new Video("MIKAN","Team Core Destruction x Multiply or Release",2000);
+        video2.AddComment(new Comment("DoodleChaos","How on earth did you script the bottom thief section to steal from each side so smoothly. The creativity of these challenges blows my mind. Amazing job keep it up!"));
+        video2.AddComment(new Comment("RomanQrr","It might be just my preference, but Release zones were a bit too big. Maybe an interesting addition would be a some kind of mechanism for the team to shrink those if only for a time?"));
+        video2.AddComment(new Comment("Naail 382","Then best detail that I notice is How Mikan, paired the complementary colours in matching abilities. Orange and cyan are complementary, likewise is Lime and Magenta; yellow and purple. But Blue and red aren't complementary, though they look totally opposite."));
+
+        Video video3 = new Video("Tier Zoo","The Fish Tier List",500);
+        video3.AddComment(new Comment("EarleyKJE","I could've gone my whole life without seeing that piranha's eyes bulge out of its head and now here I am.... Great video as always."));
+        video3.AddComment(new Comment("May Be Something","You don't play support classes to have fun, you play them to help other players pop off. The Ocean Sunfish is the support class to support classes."));
+        video3.AddComment(new Comment("BLM2295","Oh my God that clip of the elephant walking past the lion saying \"Mess around and find out\" kills me 😂"));
+
+        video1.Display();
+        video2.Display();
+        video3.Display();
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,67 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        Customer customer1 = new Customer("Kurt Dyerly");
+        Customer customer2 = new Customer("Sam Howarth");
+        Customer customer3 = new Customer("Dallin Hatton");
+
+        Address address1 = new Address();
+        address1.SetAddress("722 daisy ln");
+        address1.SetCity("Pinon hills");
+        address1.SetCountry("USA");
+        address1.SetState("CA");
+        Address address2 = new Address();
+        address2.SetAddress("156 W 4th S");
+        address2.SetCity("Rexburg");
+        address2.SetCountry("USA");
+        address2.SetState("CA");
+        Address address3 = new Address();
+        address3.SetAddress("1234 Rockforth pl");
+        address3.SetCity("Fremont");
+        address3.SetCountry("CANADA");
+        address3.SetState("Calgary");
+
+        customer1.SetAddress(address1);
+        customer2.SetAddress(address2);
+        customer3.SetAddress(address3);
+
+        Product product1 = new Product("Iphone", 354650, 999.99);
+        Product product2 = new Product("Ear Buds", 684651, 99.99);
+        Product product3 = new Product("Baby wipes", 54354, 15.75, 3);
+        Product product4 = new Product("Charlston Chews", 568432, 1.25, 36);
+        Product product5 = new Product("Duster", 354465, 57.98);
+        Product product6 = new Product("RAM 16GB", 357487, 35.99, 2);
+        Product product7 = new Product("Plush toy", 12657, 21.10, 3);
+        Product product8 = new Product("Fidget Slug", 56421, 9.55, 2);
+
+        Order order1 = new Order(customer1);
+        Order order2 = new Order(customer2);
+        Order order3 = new Order(customer3);
+
+        order1.AddProduct(product1);
+        order1.AddProduct(product2);
+        order1.AddProduct(product3);
+
+        order2.AddProduct(product6);
+        order2.AddProduct(product5);
+        order2.AddProduct(product4);
+
+        order3.AddProduct(product5);
+        order3.AddProduct(product8);
+        order3.AddProduct(product7);
+
+
+        order1.PrintPackingLabel();
+        Console.WriteLine();
+        order1.PrintShippingLabel();
+        Console.WriteLine("------------------------");
+        order2.PrintPackingLabel();
+        Console.WriteLine();
+        order2.PrintShippingLabel();
+        Console.WriteLine("------------------------");
+        order3.PrintPackingLabel();
+        Console.WriteLine();
+        order3.PrintShippingLabel();
+        Console.WriteLine("------------------------");
     }
 }
