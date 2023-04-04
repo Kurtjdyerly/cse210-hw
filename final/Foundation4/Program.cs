72d108eaa3d2d3cfc57d3aa8198430c5e32dadd7
kurtjdyerly <kurtjdyerly@gmail.com>
2023-04-04 16:39:56 -0600

    final project finished

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>();
+
+        Running run = new Running("03 Nov 2022", 30, 3);
+        StationaryBicycles bike = new StationaryBicycles("04 March 2022", 60, 20);
+        Swimming swim = new Swimming("26 Jan 2023", 120, 80);
+
+        activities.Add(run);
+        activities.Add(bike);
+        activities.Add(swim);
+
+        foreach (Activity activity in activities)
+        {
+            activity.GetSummary();
+            Console.WriteLine();
+        }
     }
 }
